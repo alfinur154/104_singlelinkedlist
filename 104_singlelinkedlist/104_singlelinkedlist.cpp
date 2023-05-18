5ad9211d1a169160769dd53f10032e561484a648
alfinur154 <alfinur27@gmail.com>
2023-05-18 19:55:14 +0700

    untuk menghapus node pada linked list

--- 104_singlelinkedlist/104_singlelinkedlist.cpp
+++ 104_singlelinkedlist/104_singlelinkedlist.cpp
@@ -58,4 +58,20 @@ bool serachNode(int nim, Node* current, Node* previous) {
 		previous = current;
 		current = current->next;
 	}
+
+	if (current == NULL)
+	{
+		return false;
+	}
+	else if (current->noMhs == nim)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+boll deleteNode(int nim) {
+
 }
